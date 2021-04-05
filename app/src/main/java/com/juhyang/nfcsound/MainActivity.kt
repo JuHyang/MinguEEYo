d47ca62ad2a5d72437264f8673382d81a05b0761
juhyang <ljh28891004@gmail.com>
2021-04-06 00:03:37 +0900

    NFC : feat(NFC) : NFC 태그 시 intent-filter 추가, 소리 재생

--- app/src/main/java/com/juhyang/nfcsound/MainActivity.kt
+++ app/src/main/java/com/juhyang/nfcsound/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.juhyang.nfcsound
 
+import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
