bc946de6cc89414335d3e28c4f70bc1d3e1bb252
juhyang <ljh28891004@gmail.com>
2021-04-11 18:38:03 +0900

    NFC : fix(NFC/manefest) : intent-filter action 추가

--- app/src/main/java/com/juhyang/nfcsound/MainActivity.kt
+++ app/src/main/java/com/juhyang/nfcsound/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.juhyang.nfcsound
 
-import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NfcAdapter
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.juhyang.nfcsound.nfc.NfcBroadcastReceiver
+
 
 class MainActivity : AppCompatActivity() {
+
+    val nfcBroadcastReceiver = NfcBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val filter = IntentFilter()
+        filter.addAction("SOME_ACTION")
+        filter.addAction("SOME_OTHER_ACTION")
+
+        registerReceiver(nfcBroadcastReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(nfcBroadcastReceiver)
+        super.onDestroy()
     }
 }
